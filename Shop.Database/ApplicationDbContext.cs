7b1487b2fe0f4ed4046710a290f9a44daf1a6ba7
Carlos <cpozosvillegas@hotmail.com>
2020-08-29 13:26:14 -0500

    Data relationships
    New tables and relationships were added

--- Shop.Database/ApplicationDbContext.cs
+++ Shop.Database/ApplicationDbContext.cs
@@ -10,5 +10,20 @@ namespace Shop.Database
          : base(options) { }      
 
       public DbSet<Product> Products { get; set; }
+      public DbSet<Stock> Stocks { get; set; }
+      public DbSet<Order> Orders { get; set; }
+      public DbSet<OrderProduct> OrderProducts { get; set; }
+
+      
+      protected override void OnModelCreating(ModelBuilder builder)
+      {
+         base.OnModelCreating(builder);
+
+         // Provides a primary key to OrderProduct table
+         // It is conformed of two keys: ProductId and OrderId. It is a composite key
+         builder.Entity<OrderProduct>()
+            .HasKey(x => new { x.ProductId, x.OrderId });
+      }
+
    }
 }
