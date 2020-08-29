ec012bd9c66cc1f657491545fdd526c7c5659c47
Carlos <cpozosvillegas@hotmail.com>
2020-08-28 20:55:17 -0500

    DbContext created
    DbContext created using two packages:
    
    Microsoft.EntityFrameworkCore
    Microsoft.AspNetCore.Identity.EntityFrameworkCore

--- Shop.Application/Products/CreateProduct.cs
+++ Shop.Application/Products/CreateProduct.cs
@@ -1,6 +1,23 @@
-﻿namespace Shop.Application.Products
+﻿using Shop.Database;
+using Shop.Domain.Models;
+
+namespace Shop.Application.Products
 {
    public class CreateProduct
    {
+      private ApplicationDbContext _context;
+
+      public CreateProduct(ApplicationDbContext dbContext)
+      {
+         _context = dbContext;
+      }
+      public void Do(string name, string description)
+      {
+         _context.Products.Add(new Product
+         {
+            Name = name,
+            Description = description
+         });
+      }
    }
 }
